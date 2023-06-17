ba9ce2967d19ffc5a594dd7279b5177c5d1bdc05
s.oganesyan <s.oganesyan@bi.zone>
2023-06-17 19:08:37 +0300

    add fullpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactFullpage from '@fullpage/react-fullpage';
+const sectionsColor = [
+  "#ff5f45",
+  "#0798ec",
+  "#fc6c7c",
+  "#435b71",
+  "orange",
+  "blue",
+  "purple",
+  "yellow"
+]
+
+const fullpages= [
+  {
+    text: "Section 1"
+  },
+  {
+    text: "Section 2"
+  },
+  {
+    text: "Section 3"
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactFullpage
+        credits={{}}
+        licenseKey={"YOUR_KEY_HERE"} // Get one from https://alvarotrigo.com/fullPage/pricing/
+        // navigation
+        anchors={["firstPage", "secondPage", "thirdPage"]}
+        // onLeave={this.onLeave.bind(this)}
+        // sectionsColor={sectionsColor}
+        render={(comp) => (
+          <ReactFullpage.Wrapper>
+            {fullpages.map(({ text }) => (
+              <div key={text} className={'section'}>
+               <p>{text}</p>
+              </div>
+            ))}
+          </ReactFullpage.Wrapper>
+        )}
+        // credits={{}}
+        // //fullpage options
+        // licenseKey = {'YOUR_KEY_HERE'}
+        // scrollingSpeed = {1000} /* Options here */
+        //
+        // render={({ state, fullpageApi }) => {
+        //   return (
+        //     <ReactFullpage.Wrapper>
+        //       <div className="section">
+        //         <Invitation />
+        //
+        //       </div>
+        //       <div className="section">
+        //         <Schedule />
+        //
+        //       </div>
+        //       <div className="section">
+        //         <Wishes />
+        //
+        //       </div>
+        //     </ReactFullpage.Wrapper>
+        //   );
+        // }}
+      />
     </div>
   );
 }
