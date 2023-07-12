7bdc33160cd7d3705df3cfb2c1bf6ac76ae4a1c1
s.oganesyan <s.oganesyan@bi.zone>
2023-07-12 19:20:51 +0300

    some commit

--- src/views/Setting/Setting.tsx
+++ src/views/Setting/Setting.tsx
@@ -9,7 +9,7 @@ export const Setting = () => {
       <div className={s.content}>
         <img className={s.flowersTopImage} src="/images/flowersBottomRight.svg" alt=""/>
         <div className={s.invitationContent}>
-          <img className={s.settingImg} src="/images/settingImg.png" alt=""/>
+          {/*<img className={s.settingImg} src="/images/settingImg.png" alt=""/>*/}
           <div className={s.text}>
             <span>*За несколько дней до свадьбы на этой странице появится информация о вашем столе</span>
           </div>
